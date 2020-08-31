d81f6320732669aa6cd5546675124f0fbb7de061
Myles Burt <myles_burt@rocketmail.com>
2020-08-31 23:34:26 +0930

    Added functions.

--- pwgen.js
+++ pwgen.js
@@ -1,7 +1,21 @@
 
+//Functions
+
 function randomLowercase () {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-
 }
 
-console.log(randomLowercase());
+function randomUppercase () {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function randomNumber () {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function randomSymbol () {
+    const symbols = `!"#$%&'()*+,-./`;
+    return symbols[Math.floor(Math.random() *symbols.length)];
+}
+
+console.log(randomSymbol());
