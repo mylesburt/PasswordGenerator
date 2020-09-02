64b06be05590185cba1c0e37b956ce9f1f2b2fdf
Myles Burt <myles_burt@rocketmail.com>
2020-09-03 01:05:23 +0930

    Trail and error with prompts.

--- pwgen.js
+++ pwgen.js
@@ -1,5 +1,16 @@
 
-//Functions
+
+// Atempted to try and do the first prompt but didn't know how to link it to the next prompt and then record the total values from all prompts to then concatenate the results together for the end result of a password.
+
+
+function promptLower(){
+    var lowerPrompt = prompt("Would you like like to use lowercase letters?");
+    alert (lowerPrompt);
+return console.log
+}
+
+
+//I found a way to seperate lowercase, uppercase, numbers and symbols into their own functions that will produce a random value.
 
 function randomLowercase () {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
